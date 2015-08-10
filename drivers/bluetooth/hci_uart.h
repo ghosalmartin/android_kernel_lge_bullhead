8f6581ad7629cd78f1fbd4b80ed63fcb6e6df890
Ben Young Tae Kim <ytkim@qca.qualcomm.com>
2015-08-10 14:24:17 -0700

    Bluetooth: hciuart: Add support QCA chipset for UART
    QCA61x4 chips have supported sleep feature using In-Band-Sleep commands
    to enable sleep feature based on H4 protocol. After sending
    patch/nvm configuration is done, IBS mode will be up and running
    
    Signed-off-by: Ben Young Tae Kim <ytkim@qca.qualcomm.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_uart.h
+++ drivers/bluetooth/hci_uart.h
@@ -35,7 +35,7 @@
 #define HCIUARTGETFLAGS		_IOR('U', 204, int)
 
 /* UART protocols */
-#define HCI_UART_MAX_PROTO	7
+#define HCI_UART_MAX_PROTO	9
 
 #define HCI_UART_H4	0
 #define HCI_UART_BCSP	1
@@ -44,6 +44,8 @@
 #define HCI_UART_LL	4
 #define HCI_UART_ATH3K	5
 #define HCI_UART_IBS	6
+#define HCI_UART_BCM	7
+#define HCI_UART_QCA	8
 
 #define HCI_UART_RAW_DEVICE	0
 #define HCI_UART_RESET_ON_INIT	1
@@ -127,4 +129,9 @@ int h5_deinit(void);
 #ifdef CONFIG_BT_HCIUART_IBS
 int ibs_init(void);
 int ibs_deinit(void);
+#endif
+
+#ifdef CONFIG_BT_HCIUART_QCA
+int qca_init(void);
+int qca_deinit(void);
 #endif
